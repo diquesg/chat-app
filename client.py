f589ecff59a8e1b019151244f4e7a8ec45c21c95
Gustavo Diques <diquesg@gmail.com>
2024-08-08 13:28:51 -0300

    The server now stays connected for new messages.

--- client.py
+++ client.py
@@ -2,23 +2,31 @@ import socket
 
 addr = ('localhost', 9051)
 
-def client(address):
-    client_socket = socket.socket()
-    print("Cliente criado. Conectando ao servidor...")
-    try:
-        client_socket.connect(address)
-    except Exception as e:
-        print(e)
-        exit()
-    print("Cliente conectado.")
-    try:
-        message = (input("Digite a mensagem a ser enviada: "))
-        print("Enviando mensagem...")
-        client_socket.sendall(message.encode("UTF-8"))
-        print("Mensagem enviada.")
-    except socket.error as e:
-        print("Erro no socket: %s" %str(e))
-    except Exception as e:
-        print("Exceção: %s" %str(e))
+def client_wrap():
+    def create_client(address):
+        print("Cliente iniciado. Conectando ao servidor...")
+        try:
+            client_socket.connect(address)
+        except Exception as e:
+            print(e)
+            exit()
+        print("Cliente conectado.")
+        return
 
-client(addr)
+    client_socket = socket.socket()
+    create_client(addr)
+
+    def client(address, client_socket):
+        try:
+            message = (input("Digite a mensagem a ser enviada: "))
+            client_socket.sendall(message.encode("UTF-8"))
+            print("Mensagem enviada.")
+        except socket.error as e:
+            print("Erro no socket: %s" %str(e))
+        except Exception as e:
+            print("Exceção: %s" %str(e))
+
+    while True:
+        client(addr, client_socket)
+
+client_wrap()
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import socket
+import threading
 
-addr = ('localhost', 9051)
+address = ('localhost', 9051)
 
-def server(address):
-    data_limit = 2048
-    print("Criando servidor...")
-    server_socket = socket.socket() #Cria um novo socket definido como "server"
-    server_socket.bind(address) #Associa o socket ao endereço do serviddor
-    server_socket.listen() #Coloca o servidor em estado de espera por solicitações
-    print("Servidor criado. Aguardando solicitações...")
-    client, address = server_socket.accept() #Aceita conexões de clientes, retornando o endereço da solicitação
-    message = client.recv(data_limit)
-    print("Mensagem: " + message.decode("UTF-8"))
-    print("Endereço: " + str(address))
+data_limit = 2048
+print("Criando servidor...")
+server_socket = socket.socket() #Cria um novo socket definido como "server_socket"
+server_socket.bind(address) #Associa o socket ao endereço do serviddor
+server_socket.listen() #Coloca o servidor em estado de espera por solicitações
+print("Servidor criado. Aguardando solicitações...")
 
-server(addr)
+def client_handler(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(data_limit).decode("UTF-8")
+            if message:
+                print(message)
+            else:
+                break
+        except Exception as e:
+            print(e)
+            break
+    client_socket.close()
+
+while True:
+    client_socket, client_address = server_socket.accept() #Aceita conexões de clientes, retornando o endereço da solicitação
+    print("Nova conexão")
+    client_thread = threading.Thread(target=client_handler(client_socket))
+    client_thread.start()
